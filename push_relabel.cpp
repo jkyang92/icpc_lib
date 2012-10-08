cecfd12a886f315298c652182968745953302702
Jay Yang <jkelleyy@gmail.com>
2012-10-07 23:39:31 -0500

    property implement the highest active vertex selection rule for push-relabel

--- push_relabel.cpp
+++ push_relabel.cpp
@@ -1,23 +1,18 @@
 #include <cstring>
 #include <climits>
-#include <queue>
 #include <vector>
+#include <list>
 
 using namespace std;
 
-bool cmp(const pair<int,int>& a, const pair<int,int>& b){
-    if(a.second==b.second)
-        return a.first<b.first;
-    return a.second<b.second;
-}
 /* T should be some sort of two dimensional array
  * capacity[i][i]=0 for all i
  * flow matrix must be zeroed
  * the start node is assumed to be node 0, and the end node node_count-1
  * IN: capacity node_count
  * OUT: flow
- * Algorithm: push-relabel (relabel to front)
- * Time Complexity: O(V^3) (V=node_count)
+ * Algorithm: push-relabel (highest active vertex)
+ * Time Complexity: O(V^2 sqrt(E)) (V=node_count,E=edges)
  * Space Complexity: O(V)
  */
 template<typename T>
@@ -29,6 +24,10 @@ void maxflow(T capacity,int node_count,T flow) {
     memset(excess,0,sizeof(int)*node_count);
     memset(start,0,sizeof(int)*node_count);
     heights[0]=node_count;
+    int top_height = 0;
+    int remain=0;
+    list<int> *height_array = new list<int>[node_count*2];
+    bool *in_queue = new bool[node_count];
     vector<int>* adj_list = new vector<int>[node_count];
     for(int i=0;i<node_count;i++){
         for(int j=i+1;j<node_count;j++){
@@ -39,18 +38,25 @@ void maxflow(T capacity,int node_count,T flow) {
         }
     }
     //setup outbound flow from v
-    priority_queue<pair<int,int>,vector<pair<int,int> >,bool(*)(const pair<int,int>&,const pair<int,int>&)> todo(cmp);
     for(int i=0;i<node_count;i++){
         flow[0][i]=capacity[0][i];
         excess[i] = capacity[0][i];
         if(excess[i]!=0){
-            if(i!=0 && i!=node_count-1)
-                todo.push(make_pair(i,heights[i]));
+            if(i!=0 && i!=node_count-1){
+                remain++;
+                in_queue[i]=true;
+                height_array[heights[i]].push_back(i);
+            }
         }
     }
-    while(!todo.empty()){
-        int curr = todo.top().first;
-        todo.pop();
+    while(remain>0){
+        int curr = height_array[top_height].front();
+        height_array[top_height].pop_front();
+        in_queue[curr]=false;
+        remain--;
+        while(height_array[top_height].empty() && top_height>0){
+            top_height--;
+        }
         while(excess[curr]>0){
             int curr_height = heights[curr];
             if(start[curr]<adj_list[curr].size()){
@@ -61,8 +67,14 @@ void maxflow(T capacity,int node_count,T flow) {
                         int nflow = min(res,excess[curr]);
                         excess[curr]-=nflow;
                         excess[i]+=nflow;
-                        if(i!=0 && i!=node_count-1)
-                            todo.push(make_pair(i,heights[i]));
+                        if(!in_queue[i] && i!=0 && i!=node_count-1){
+                            remain++;
+                            in_queue[i]=true;
+                            height_array[heights[i]].push_back(i);
+                            if(heights[i]>top_height){
+                                top_height=heights[i];
+                            }
+                        }
                         int removed = min(flow[i][curr],nflow);
                         flow[i][curr]-=removed;
                         nflow-=removed;
@@ -80,11 +92,12 @@ void maxflow(T capacity,int node_count,T flow) {
                         best = heights[other];
                     }
                 }
-                assert(best!=INT_MAX);
                 heights[curr]=best+1;
             }
         }
     }
+    delete[] height_array;
+    delete[] in_queue;
     delete[] adj_list;
     delete[] heights;
     delete[] excess;
