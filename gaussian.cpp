106108117d813cd91792148c8e63f5ad890cebb7
Jay Yang <jkelleyy@gmail.com>
2012-10-19 16:09:46 -0500

    minify gaussian.cpp

--- gaussian.cpp
+++ gaussian.cpp
@@ -21,33 +21,30 @@ vector<int> gaussian_elimination(T** A,int n, int m){
     for(int c=0;c<m;c++){
         //find piviot
         int pivot = -1;
-        T pivotValue = 0;
+        T p_value = 0;
         for(int r=p_row;r<n;r++){
             if(abs(A[r][c])>pivotValue){
-                pivotValue = abs(A[r][c]);
+                p_value = abs(A[r][c]);
                 pivot = r;
             }
         }
-        if(pivotValue==0){
+        if(p_value==0){
             ret.push_back(c);
             continue;
         }
-        if(p_row!=pivot)
-            swap_ranges(A[p_row],A[p_row]+n,A[pivot]);
-        pivotValue = A[p_row][c];
+        if(p_row!=pivot) swap_ranges(A[p_row],A[p_row]+n,A[pivot]);
+        p_value = A[p_row][c];
         for(int r=0;r<n;r++){
-            if(r==c)
-                continue;
-            T multiplier = A[r][c]/pivotValue;
-            if(multiplier!=0)
+            if(r==c) continue;
+            T mult = A[r][c]/p_value;
+            if(mult!=0)
                 for(int i=c+1;i<m;i++)
-                    A[r][i]-=A[r][i]*multiplier;
+                    A[r][i]-=A[r][i]*mult;
             A[r][c]=0;
         }
         //set pivot to 1
         A[p_row][c]=1;
-        for(int i=p_row+1;i<m;i++)
-            A[p_row][i]/=pivotValue;
+        for(int i=p_row+1;i<m;i++) A[p_row][i]/=p_value;
         p_row++;
     }
     return ret;
@@ -62,16 +59,14 @@ vector<T*> find_homogeneous_solutions(T** A,int n){
     vector<int> pivots;
     int index = 0;
     for(int i=0;i<n;i++){
-        if(index>=non_pivots.size() || non_pivots[index]==i)
-            index++;
-        else
-            pivots.push_back(i);
+        if(index>=non_pivotes.size()) break;
+        if(non_pivots[index]==i) index++;
+        else pivots.push_back(i);
     }
     ret.resize(non_pivots.size());
     for(int i=0;i<ret.size();i++){
         ret[i] = new T[n];
-        for(int j=0;j<n;j++)
-            ret[i][j]=0;
+        fill_n(ret[i],n,0);
         for(int j=0;j<n-non_pivots.size();j++)
             ret[i][pivots[j]]=-A[non_pivots[i]][j];
         ret[i][non_pivots[i]]=1;
