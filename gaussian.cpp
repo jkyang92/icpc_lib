14a1a543f6132467d5c3b50235008adbb5d1a210
Jay Yang <jkelleyy@gmail.com>
2012-10-20 11:58:24 -0500

    fix typoes and add determinant calcuation

--- gaussian.cpp
+++ gaussian.cpp
@@ -15,24 +15,29 @@ using namespace std;
  *returns the non-pivot columns
  */
 template<typename T>
-vector<int> gaussian_elimination(T** A,int n, int m){
+vector<int> gaussian_elimination(T** A,int n, int m,T *pdet){
     vector<int> ret;
     int p_row=0;
+    int det=1;
     for(int c=0;c<m;c++){
         //find piviot
         int pivot = -1;
         T p_value = 0;
         for(int r=p_row;r<n;r++){
-            if(abs(A[r][c])>pivotValue){
+            if(abs(A[r][c])>p_value){
                 p_value = abs(A[r][c]);
                 pivot = r;
             }
         }
         if(p_value==0){
             ret.push_back(c);
+            det = 0;
             continue;
         }
-        if(p_row!=pivot) swap_ranges(A[p_row],A[p_row]+n,A[pivot]);
+        if(p_row!=pivot){
+            swap_ranges(A[p_row],A[p_row]+n,A[pivot]);
+            det*=((p_row-pivot)%2==0 ? 1 : -1);
+        }
         p_value = A[p_row][c];
         for(int r=0;r<n;r++){
             if(r==c) continue;
@@ -45,8 +50,11 @@ vector<int> gaussian_elimination(T** A,int n, int m){
         //set pivot to 1
         A[p_row][c]=1;
         for(int i=p_row+1;i<m;i++) A[p_row][i]/=p_value;
+        det*=p_value;
         p_row++;
     }
+    if(pdet)
+        *pdet=det;
     return ret;
 }
 
@@ -55,11 +63,11 @@ vector<int> gaussian_elimination(T** A,int n, int m){
 template<typename T>
 vector<T*> find_homogeneous_solutions(T** A,int n){
     vector<T*> ret;
-    vector<int> non_pivots = gaussian_elimination(A,n,n);
+    vector<int> non_pivots = gaussian_elimination(A,n,n,(double*)0);
     vector<int> pivots;
     int index = 0;
     for(int i=0;i<n;i++){
-        if(index>=non_pivotes.size()) break;
+        if(index>=non_pivots.size()) break;
         if(non_pivots[index]==i) index++;
         else pivots.push_back(i);
     }
