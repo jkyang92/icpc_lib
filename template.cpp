a7a350aeea24b396428f7de8f4ab1ce94d26e4b8
Jay Yang <jkelleyy@gmail.com>
2013-03-03 12:11:50 -0600

    main file template

---
+++ template.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <deque>
+#include <list>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <bitset>
+#include <string>
+#include <algorithm>
+#include <utility>
+#include <functional>
+#include <iterator>
+#include <complex>
+#include <valarray>
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <cctype>
+#include <cassert>
+#include <cstring>
+
+using namespace std;
