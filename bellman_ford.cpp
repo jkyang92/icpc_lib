167d1573cb9782e9a78aa2395af1e578b36110e6
Jay Yang <jkelleyy@gmail.com>
2013-03-03 17:15:57 -0600

    fix bellman_ford

--- bellman_ford.cpp
+++ bellman_ford.cpp
@@ -6,7 +6,7 @@ struct edge{
     int a,b,cost;
 };
 
-void bellman_ford(int ncount,vector<edge> edges,int source,int *dist, bool *reachable){
+void bellman_ford(int ncount,vector<edge> edges,int source,int *dist, bool *reachable, int *pred){
     for(int i=0;i<ncount;i++){
         dist[i]=-1;
         reachable[i]=false;
@@ -15,10 +15,13 @@ void bellman_ford(int ncount,vector<edge> edges,int source,int *dist, bool *reac
     reachable[source]=true;
     for(int i=0;i<ncount-1;i++){
         for(int j=0;j<edges.size();j++){
-            int ncost = edges[j].cost+dist[edges[j].a];
-            if(!reachable[edges[j].b] || ncost < dist[edges[j].b]){
-                dist[edges[j].b]=ncost;
-                reachable[edges[j].b]=true;
+            if(reachable[edges[j].b]){
+                int ncost = edges[j].cost+dist[edges[j].a];
+                if(!reachable[edges[j].b] || ncost < dist[edges[j].b]){
+                    dist[edges[j].b]=ncost;
+                    reachable[edges[j].b]=true;
+                    pred[edges[j].b] = edges[j].a;
+                }
             }
         }
     }
