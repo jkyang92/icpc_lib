7c771eeaebc2c499079bc9a7733bf71bdd05cc15
Jay Yang <jkelleyy@gmail.com>
2013-03-09 10:51:31 -0600

    fix the script again

--- util/code_fix.sh
+++ util/code_fix.sh
@@ -3,7 +3,7 @@
 
 for file in `cat ./files.lst`
 do
-sed 's/[\t]/    /' < "$file" > "$file.new"
-diff "$file" "$file.new" > /dev/null || (mv "$file" "$file.old" && mv "$file.new" "$file");
+sed 's/[\t]/    /g' < "$file" > "$file.new"
+diff "$file" "$file.new" > /dev/null || (mv "$file" "$file.old" && cp "$file.new" "$file");
 rm "$file.new";
 done
