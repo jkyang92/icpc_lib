95c942100ca0ef02316a0d76d57703c084d5368c
Jay Yang <jkelleyy@gmail.com>
2013-03-09 10:43:32 -0600

    add indent fixing utility

---
+++ util/code_fix.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+
+for file in `cat ./files.lst`
+do
+sed 's/[\t]/    /' < "$file" > "$file.new"
+diff "$file" "$file.new" > /dev/null || (mv "$file" "$file.old" && mv "$file.new" "$file" && rm "$file.new");
+done
