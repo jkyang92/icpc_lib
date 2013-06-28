351915f50a25574748e513ca30e4c6accfd6d29a
Eric Kulcyk <erickulcyk@gmail.com>
2013-06-28 08:26:46 -0500

    minor mods to mcmf and simplified edit distance

--- EditDistance.cpp
+++ EditDistance.cpp
@@ -1,84 +1,91 @@
 #include <stdio.h>
 #include <string.h>
 
-void print(int o,int t);
-
-int sol[25][25];
-int next [25][25];
+const int strLen = 25;
+int sol[strLen][strLen];
+int next [strLen][strLen];
 
 char* one,*two;
 int n,m;
 int numDeleted;
 
-int main(){
-    while(true){
-        one = new char[25];
-        two = new char[25];
-        scanf("%s",one);
-        if(one[0]=='#')
-            break;
-        scanf("%s",two);
-        n = strlen(one);
-        m = strlen(two);
-        for(int i = m; i>=0; i--){
-            sol[n][i]=m-i;
-        }
-        for(int i = n-1; i>=0; i--){
-            sol[i][m]=n-i;
-        }
-        for(int o = n-1; o>=0; o--){
-            for(int t = m-1; t>=0; t--){
-                sol[o][t] = one[o]==two[t]?sol[o+1][t+1]:sol[o+1][t+1]+1;
-                sol[o][t] = sol[o][t]< sol[o][t+1]+1? sol[o][t] : sol[o][t+1]+1;
-                sol[o][t] = sol[o][t]< sol[o+1][t]+1? sol[o][t] : sol[o+1][t]+1;
-                next[o][t]= sol[o][t]==sol[o][t+1]+1 ? 1
-                                       :(sol[o][t]==sol[o+1][t]+1?2:0);
-                next[o][t]= next[o][t]==0 && one[o]==two[t]?-1:next[o][t];
-            }
-        }
-        numDeleted = 0;
-        print(0,0);
-    }
-    return 0;
+int solve()
+{
+	n = strlen(one);
+	m = strlen(two);
+	for(int i = m; i>=0; i--){
+		sol[n][i]=m-i;
+	}
+	for(int i = n-1; i>=0; i--){
+		sol[i][m]=n-i;
+	}
+	for(int o = n-1; o>=0; o--){
+		for(int t = m-1; t>=0; t--){
+			sol[o][t] = one[o]==two[t]?sol[o+1][t+1]:sol[o+1][t+1]+1;
+			sol[o][t] = sol[o][t]< sol[o][t+1]+1? sol[o][t] : sol[o][t+1]+1;
+			sol[o][t] = sol[o][t]< sol[o+1][t]+1? sol[o][t] : sol[o+1][t]+1;
+			next[o][t]= sol[o][t]==sol[o][t+1]+1 ? 1
+				:(sol[o][t]==sol[o+1][t]+1?2:0);
+			next[o][t]= next[o][t]==0 && one[o]==two[t]?-1:next[o][t];
+		}
+	}
+	return sol[0][0];
 }
-
+//Prints out how to transform one string to the other.
 void print(int o,int t){
-    if(o==n && t==m){
-        printf("E\n");
-    }
-    else if (o==n){
-        for(int i = t; i <m; i++){
-            printf("I%c%02d",two[i],(o+i-t-numDeleted+1));
-        }
-        printf("E\n");
-    }
-    else if (t==m){
-        for(int i = o; i <n; i++)
-            printf("D%c%02d",one[i],(o-numDeleted+1));
-        printf("E\n");
-    }
-    else {
-        switch (next[o][t]){
-        case 0:
-            printf("C%c%02d",two[t],(o-numDeleted+1));
-            print(o+1,t+1);
-            break;
-        case -1:
-            print(o+1,t+1);
-            break;
-        case 1:
-            printf("I%c%02d",two[t],(o-numDeleted+1));
-            numDeleted--;
-            print(o,t+1);
-            break;
-        case 2:
-            printf("D%c%02d",one[o],(o-numDeleted+1));
-            numDeleted++;
-            print(o+1,t);
-            break;
-        default:
-            printf("E\n");
-            break;
-        }
-    }
+	if(o==n && t==m){
+		printf("E\n");//END
+	}
+	else if (o==n){
+		for(int i = t; i <m; i++){
+			printf("I%c%02d",two[i],(o+i-t-numDeleted+1));//INSERT
+		}
+		printf("E\n");//END
+	}
+	else if (t==m){
+		for(int i = o; i <n; i++)
+			printf("D%c%02d",one[i],(o-numDeleted+1));//DELETE
+		printf("E\n");
+	}
+	else {
+		switch (next[o][t]){
+			case 0:
+				printf("C%c%02d",two[t],(o-numDeleted+1));//CHANGE
+				print(o+1,t+1);
+				break;
+			case -1:
+				print(o+1,t+1);//ALL GOOD
+				break;
+			case 1:
+				printf("I%c%02d",two[t],(o-numDeleted+1));//INSERT
+				numDeleted--;
+				print(o,t+1);
+				break;
+			case 2:
+				printf("D%c%02d",one[o],(o-numDeleted+1));//DELETE
+				numDeleted++;
+				print(o+1,t);
+				break;
+			default:
+				printf("E\n");//END
+				break;
+		}
+	}
+}
+void print(){
+	numDeleted = 0;
+	print(0,0);
+}
+int main(){
+	while(true){
+		one = new char[strLen];
+		two = new char[strLen];
+		scanf("%s",one);
+		if(one[0]=='#')
+			break;
+		scanf("%s",two);
+		printf("num changed %d\n",solve());
+		print();
+	}
+	return 0;
 }
--- minCostMaxFlow.cpp
+++ minCostMaxFlow.cpp
@@ -9,132 +9,125 @@ using namespace std;
 #define f(i,n) for(int i =0;i<n;i++)
 #define fi(i,t,n) for(t::iterator i=n.begin(); i!=n.end();i++)
 
-#define outI(i) (::begin+nnodes+(i))
 #define indx(i) (::begin+(i))
 //COST LINE NOT NEEDED FOR MAX FLOW
-#define addE(i,j,cap,cos) do{                   \
-        nodes[i][j]=cap; nodes[j][i]=0;         \
-        cost[i][j]=cos;  cost[j][i]=-(cos);     \
-    }while(false)
+#define addE(i,j,cap,cos) do{\
+	nodes[i][j]=cap; nodes[j][i]=0;\
+	cost[i][j]=cos;  cost[j][i]=-(cos);\
+}while(false)
 #define loopP() for(int c=sink, p=prevs[c]; c!=source; c=p, p=prevs[p])
 #define clearN(i) for_each(i,i+mn,mem_fun_ref(&node::clear))
-
+typedef long long ll;
+const ll big = 1E17;
 const int mn = 300;//MAX NODES
 typedef map<int,int> node;//stores [to,cap]
 node nodes[mn];
-int prevs[mn],pot[mn];//previous,potential
-typedef pair<int,int> edg;//edge, stores [cost,to]
+int prevs[mn];
+ll pot[mn];//previous,potential
+typedef pair<ll,int> edg;//edge, stores [cost,to]
 int source=0, sink=1, begin=2, nnodes;//# of nodes (set at runtime)
 //BEGIN NOT NEEDED FOR MAX FLOW//
 node reCost[mn],cost[mn];//reducedCost, cost.
 void reduce(){
-    f(i,nnodes)
-        fi(k,node,nodes[i]){
-            int j = k->first;
-            if(k->second>0){
-                reCost[i][j] += pot[i] - pot[j];
-                reCost[j][i]=0;
-            }
-        }
+	f(i,nnodes)
+		fi(k,node,nodes[i]){
+			int j = k->first;
+			if(k->second>0){
+				reCost[i][j] += pot[i] - pot[j];
+				reCost[j][i]=0;
+			}
+		}
 }
 //END NOT NEEDED FOR MAX FLOW//
 int d(){
-    fill(prevs,prevs+nnodes,-1);
-    fill(pot, pot+nnodes, INT_MAX/2);
-    //FOR MF, YOU CAN USE JUST A QUEUE OF INTS
-    priority_queue<edg, vector<edg>, greater<edg> >q;
-    pot[source] = 0;
-    q.push(edg(0,source));
-    while(!q.empty()){
-        edg c = q.top();q.pop();
-        int cur = c.second, cost = c.first;
-        if(cur==sink)return pot[sink];
-        if(pot[cur] < cost)continue;
-        fi(i,node,nodes[cur]){//iterate for begin to end
-            int to = i->first;
-            if(i->second<=0)continue;
-            if(pot[to] <= reCost[cur][to] + pot[cur])continue;
-            pot[to] = pot[cur]+reCost[cur][to];
-            q.push(edg(pot[to],to));
-            prevs[to] = cur;
-        }
-    }
-    reduce();
-    return -1;
+	fill(prevs,prevs+nnodes,-1);
+	fill(pot, pot+nnodes, big);
+	priority_queue<edg, vector<edg>, greater<edg> >q;//FOR MF, YOU CAN USE JUST A QUEUE OF INTS
+	pot[source] = 0;
+	q.push(edg(0,source));
+	while(!q.empty()){
+		edg c = q.top();q.pop();
+		int cur = c.second, cost = c.first;
+		if(cur==sink)return pot[sink];
+		if(pot[cur] < cost)continue;
+		fi(i,node,nodes[cur]){//iterate for begin to end
+			int to = i->first;
+			if(i->second<=0)continue;
+			if(pot[to] <= reCost[cur][to] + pot[cur])continue;
+			pot[to] = pot[cur]+reCost[cur][to];
+			q.push(edg(pot[to],to));
+			prevs[to] = cur;
+		}
+	}
+	reduce();
+	return -1;
 }
 
-int update(int &cst){
-    int ret = INT_MAX;
-    loopP() {ret = min(ret, nodes[p][c]);}
-    //cst+= NOT NEEDED FOR MF
-    loopP() {nodes[p][c] -= ret; nodes[c][p] += ret; cst += cost[p][c] * ret;}
-    return ret;
+int update(ll &cst, int left){
+	int flow = INT_MAX;
+	loopP() {flow = min(flow, nodes[p][c]);}
+	flow = min(left,flow);
+	loopP() {nodes[p][c] -= flow; nodes[c][p] += flow; cst += cost[p][c] * flow;}//cst+= NOT NEEDED FOR MF
+	return flow;
 }
 
-int maxFlow(int& c){
-    c=0;//c=cost,    //BEGIN NOT NEEDED FOR MF
-    fill(pot, pot+nnodes, INT_MAX/2);
-    copy(cost, cost+nnodes, reCost);
-    pot[source]=0;
-    f(k,nnodes)
-        f(i,nnodes){
-            fi(j,node,nodes[i]){
-                int to = j->first;
-                //make sure the edge is not a backwards residual edge
-                //in the graph.
-                if(j->second>0)
-                    pot[to] = min (pot[to], pot[i]+cost[i][to]);
-            }
-        }
-    reduce();//END NOT NEEDED FOR MF//
-    int flow=0;
-    int loop = 0;
-    while(d()>=0 && loop<1000){loop++;flow+=update(c);}
-    return flow;
+int maxFlow(ll& c, int dd){
+	c=0;//c=cost,	//BEGIN NOT NEEDED FOR MF
+	fill(pot, pot+nnodes, big);
+	copy(cost, cost+nnodes, reCost);
+	pot[source]=0;
+	f(k,nnodes)
+		f(i,nnodes){
+			fi(j,node,nodes[i]){
+				int to = j->first;
+				if(j->second>0)//make sure the edge is not a backwards residual edge in the graph.
+					pot[to] = min (pot[to], pot[i]+cost[i][to]);
+			}
+		}
+	reduce();//END NOT NEEDED FOR MF//
+	int flow=0, left = dd;
+	while(d()>=0 && left>0){int f=update(c,left);flow+=f;left-=f;}
+	return flow;
 }
+
 
 int main(){//addE(i,j,cap,cost)
-    while(true){//n=num postings, m=num people
-        int n,m;
-        clearN(nodes);
-        clearN(reCost);//NOT NEEDED FOR MF
-        scanf("%d %d",&n,&m);
-        nnodes = n+m+2;
-        if(n==0&&m==0)
-            return 0;
-        f(i,n){
-            int pos;
-            scanf("%d",&pos);
-            addE(source,indx(i),pos,0);
-        }
-        int co[3][4]={{4,3,2,1},{8,7,6,5},{12,11,10,9}};
-        f(i,m){
-            int y,cs[4];
-            scanf("%d %d %d %d %d",&y,cs,cs+1,cs+2,cs+3);
-            f(j,4)
-                addE(indx(cs[j]),indx(n+i),1,-co[y-1][j]);
-            addE(indx(n+i),sink,1,0);
-        }
-        int cst;
-        maxFlow(cst);
-        printf("%d\n",-cst);
-    }
+	while(true){//n=num postings, m=num people
+		int n,m;
+		clearN(nodes);
+		clearN(reCost);//NOT NEEDED FOR MF
+		if(scanf("%d %d",&n,&m)==EOF)return 0;
 
-    /*
-       nnodes = 5;
-
-       addE(source,indx(0),5,0);
-       addE(indx(0),indx(1),7,1);
-       addE(indx(0),indx(2),7,5);
-       addE(indx(1),indx(2),2,-2);
-       addE(indx(1),indx(3),3,8);
-       addE(indx(2),indx(3),3,-3);
-       addE(indx(3),sink,3,0);
-       addE(indx(2),indx(4),2,4);
-       addE(indx(4),sink,2,0);
-
-       int cost,mf;
-       mf=maxFlow(cost);
-       printf("cost:%d %d\n",cost,mf);
-       */    return 0;
+		nnodes = 2*n+2;
+		int graph[100][100];
+		f(i,n)f(j,n)graph[i][j]=-1;
+		//printf("reading graph\n");
+		f(i,m){
+			int a,b,cos;
+			scanf("%d %d %d",&a,&b,&cos);
+			graph[a-1][b-1]=cos;
+		}
+		int k,d;
+		scanf("%d %d",&d,&k);
+		//printf("adding edges\n");
+		f(i,n)
+			addE(indx(i),indx(n+i),k,0);
+		f(i,n)
+			f(j,n)
+				if(graph[i][j]!=-1){
+					addE(indx(n+i),indx(j),k,graph[i][j]);
+					addE(indx(j+n),indx(n),k,graph[i][j]);
+					//printf("adding from %d to %d and %d to %d and %d to %d\n",i,j,j,n+i*n+j,n+i*n+j,i);
+				}
+		addE(source,indx(0),1E9,0);
+		addE(indx(n-1),sink,1E9,0);
+		ll cst;
+		//printf("starting mf\n");
+		int flow = maxFlow(cst,d);
+		if(flow==d)
+			printf("%d\n",cst);
+		else
+			printf("Impossible.\n");
+	}
+	return 0;
 }
